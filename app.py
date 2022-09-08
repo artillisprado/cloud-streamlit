4564ffac484158628303eb9f936c697c7905ca8e
aadejarde15@gmail.com <aadejarde15@gmail.com>
2022-09-08 14:36:51 -0300

    atualizações do pytest

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 import streamlit as st
 
-def test_executable():
-    st.set_page_config(
-        page_title = "Teste do Streamlit por Gabriel Alves",
-        layout = "wide",
-        menu_items = {
-            'About': "TESTE DO ABOUT"
-        }
-    )
+st.set_page_config(
+    page_title = "Teste do Streamlit por Gabriel Alves",
+    layout = "wide",
+    menu_items = {
+        'About': "TESTE DO ABOUT"
+    }
+)
 
-    nome = st.text_input('Digite o seu nome:')
-    if nome == '':
-        st.markdown('O STREAMLIT RODOU!')
-    else:
-        st.markdown(f'''
-            Oi, <b style="color:#0033FF; text-transform:uppercase">{nome}</b>.<br>
-            O STREAMLIT RODOU!<br>
-            E ele aceita <code>HTML</code> e <code>CSS</code>!
-        ''', unsafe_allow_html=True)
-
-test_executable()
+nome = st.text_input('Digite o seu nome:')
+if nome == '':
+    st.markdown('O STREAMLIT RODOU!')
+else:
+    st.markdown(f'''
+        Oi, <b style="color:#0033FF; text-transform:uppercase">{nome}</b>.<br>
+        O STREAMLIT RODOU!<br>
+        E ele aceita <code>HTML</code> e <code>CSS</code>!
+    ''', unsafe_allow_html=True)
